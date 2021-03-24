0abd9b55a1a3b555850603329a75d459ff396637
SergiuBic <wikin_n@yahoo.com>
2021-03-24 10:22:35 +0200

    DPR v0.1 - Init

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import SignatureCanvas from 'react-signature-canvas'
+import h2c from 'html2canvas'
+import jsPDF from 'jspdf'
+import "react-datepicker/dist/react-datepicker.css";
+
+function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [selectValue, setSelectedValue] = useState(0);
+  const [signatureDone, setSignature] = useState(false);
+  const [trimmedDataURL, setTrimmedData] = useState(null);
+  const [textSelected, setTextSelected] = useState("");
+  const [alteMotive, setAlteMotive] = useState("");
+  const [mainName, setMainName] = useState("");
+
+
+
+  let sigPad = {}
+  function toggleMainName(e) {
+    setMainName(e.target.value);
+  }
+  function toggleAlteMotive(e) {
+    setAlteMotive(e.target.value);
+  }
+  function toggleMotive(e) {
+    setSelectedValue(e.target.value);
+    setTextSelected(e.target.options[e.target.selectedIndex].text)
+  }
+  function toggleSignature(e) {
+    e.preventDefault();
+    setSignature(true);
+    
+    setTrimmedData(sigPad.getTrimmedCanvas()
+        .toDataURL('image/png'))
+
+    e.target.classList.add("hidden")
+
+    const input = document.getElementById('react-app');
+    setTimeout(function(){
+
+      h2c(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF( {
+        unit: "in",
+        format: [11, 10]
+      });
+      pdf.addImage(imgData, 'PNG', 0, 0);
+      pdf.save(`${mainName} - ${startDate.toLocaleString('ro-RO')} - Declaratie.pdf`); }
+        );
+        
+    },500);
+
+  }
+  return (
+    <div className="App m-0" id="react-app">
+      <div className="mx-2 pa-2 text-xl text-left box-content border-1 border-gray-100 rounded-xl p-4 w-2/4">
+         <h2 className="font-bold"> Declarație pe propria răspundere </h2>
+         <h5 className="text-sm"> - pentru deplasarea în perioada carantinării zonale - </h5>
+      </div>
+  <form id="react-form" className="flex flex-col flex-nowrap m-auto justify-start w-auto mt-2 ml-4 space-y-1">
+
+<div>
+<label  htmlFor="input1" >Subsemnata/subsemnatul:</label>
+<input name="input1" type="text" onKeyUp={toggleMainName} className="focus:outline-none ml-2 w-auto border-2 border-gray-200 rounded-xl p-2" />
+</div>
+<div>
+<label  htmlFor="input2" >Născută/născut la data de:</label>
+<input name="input2" type="text"  className="focus:outline-none ml-2 w-auto border-2 border-gray-200 rounded-xl p-2" />
+</div>
+<div>
+<label  htmlFor="input3">Având domiciliul/reședința:</label>
+<input name="input3" type="text"  className="focus:outline-none ml-2 w-auto border-2 border-gray-200 rounded-xl p-2" />
+</div>
+
+
+{signatureDone ? <div className="w-1/2"><input type='checkbox' className="mr-2" checked /><label className="text-xl bold">{textSelected}</label>{selectValue === '14' ? <div className='focus:outline-none  border-2 rounded-xl border-gray-700 mt-2 p-2 w-1/2' >{alteMotive}</div> : `-`}
+</div> : <div>
+<select id="react-select" name="myselect" className="focus:outline-none w-1/4 border-2 border-gray-700 rounded-xl p-2 mt-2"
+  defaultValue={selectValue} 
+  onChange={toggleMotive} >
+<option value="0"  disabled>Alege motivul</option>
+<option value="1">deplasarea în interes profesional, inclusiv între locuință, gospodărie și locul/locurile de desfășurare a activității profesionale și înapoi;</option>
+<option value="2">deplasarea pentru asigurarea de bunuri care acoperă necesitățile de bază ale persoanelor și animalelor de companie/domestice, precum și bunuri necesare desfășurării activității profesionale, pentru persoanele cu vârsta de peste 65 de ani în intervalul 10:00-13:00, iar pentru persoanele cu vârsta mai mică de 65 de ani în intervalele orare 6:00-10:00, respectiv 13:00-20:00;</option>
+<option value="3">deplasarea pentru asistență medicală care nu poate fi amânată și nici realizată de la distanță;</option>
+<option value="4">deplasările scurte în apropierea locuinței/gospodăriei legate de activitatea fizică individuală a persoanelor (cu excluderea oricăror activități sportive de echipă), precum și pentru nevoile animalelor de companie/domestice;</option>
+<option value="5">deplasarea în scopul donării de sânge la centrele de transfuzie sanguină;</option>
+<option value="6">deplasarea în scop umanitar sau de voluntariat;</option>
+<option value="7">deplasarea pentru realizarea de activități agricole;</option>
+<option value="8">deplasarea producătorilor agricoli pentru comercializarea de produse agroalimentare;</option>
+<option value="9">îngrijirea sau administrarea unei proprietăți din altă localitate și/sau eliberarea de documente necesare pentru obținerea unor drepturi;</option>
+<option value="10">deplasare în vederea susținerii examenelor pentru obținerea permisului de conducere;</option>
+<option value="11">participarea la programe sau proceduri în centrele de tratament;</option>
+<option value="12">pentru achiziția, service-ul, efectuarea ITP sau alte operațiuni de întreținere a vehiculelor, activități care nu pot fi efectuate în localitatea de domiciliu, cu prezentarea unui document justificativ;</option>
+<option value="13">alte motive justificative precum: îngrijirea/însoțirea copiilor/membrilor de familie, îngrijirea unei/unui rude/afin sau persoane aflate în întreținere, asistența persoanelor vârstnice, bolnave sau cu dizabilități ori deces al unui membru de familie;</option>
+<option value="14">Alte motive legale</option>
+</select>
+<div>
+{selectValue === '14' ? <input type="text" className='focus:outline-none  border-2 rounded-xl border-gray-700 mt-2 p-2' placeholder="Precizați motivul" onKeyUp={toggleAlteMotive}/> : `-`}
+</div>
+</div>}
+
+
+<div>
+<label htmlFor="adr">Adresa spre care se deplasează:</label>
+<input name="adr" type="text"  className="focus:outline-none ml-2  border-2 border-gray-200 rounded-xl p-2" />
+</div>
+<div>
+<label htmlFor="iorar">Intervalul orar în care se efectuează deplasarea:</label>
+<input name="iorar" type="text"  className="focus:outline-none ml-2  border-2 border-gray-200 rounded-xl p-2" />
+</div>
+<div>
+<label htmlFor="contact" className='text-sm bold'>Datele de identificare și de contact ale persoanei căreia i se acordă îngrijirea:</label>
+<input name="contact" type="text"  className="focus:outline-none ml-2  border-2 border-gray-200 rounded-xl p-2" />
+</div>
+<div className="italic text-sm w-1/3">Subsemnata/subsemnatul cunosc prevederile art. 326 din Codul penal cu privire la falsul în declarații coroborat cu art. 352 din Codul penal referitor la zădărnicirea combaterii bolilor.</div>
+
+
+<div className="footer flex flex-row flex-nowrap justify-around align-left w-1/3">
+  <div className="mr-12">Semnătura 
+    
+    {signatureDone ? <img className="w-auto" alt="GeneratedSignature - Declaratie"
+          src={trimmedDataURL} /> : <SignatureCanvas penColor='black'
+    canvasProps={{width: 200, height: 200, className: 'sigCanvas border-2 border-gray-700 rounded-xl'}} ref={(ref) => { sigPad = ref }} />}
+    </div>
+
+  <div>Data <DatePicker selected={startDate} onChange={date => setStartDate(date)} className="focus:outline-none  border-2 border-gray-700 rounded-xl p-2" /></div>
+</div>
+      </form>
+
+      <input type="button" id="generate" value="Generează" onClick={toggleSignature}
+      className="bg-blue-200 p-3 mt-2 mb-2 m-auto flex text-center rounded-xl border-2 border-gray-500"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Generated.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export default function Generated() {
+    return (
+        <div>
+            <input type="checkbox" name="cb1" />
+<label for="cb1" >deplasarea în <b>interes profesional</b>, inclusiv între locuință, gospodărie și locul/locurile de desfășurare a activității profesionale și înapoi; </label>
+
+<input type="checkbox" name="cb2" />
+<label for="cb2" >deplasarea pentru <b>asigurarea de bunuri</b> care acoperă necesitățile de bază ale persoanelor
+și animalelor de companie/domestice, precum și bunuri necesare desfășurării activității
+profesionale, pentru persoanele cu vârsta de peste 65 de ani în intervalul 10:00-13:00, iar
+pentru persoanele cu vârsta mai mică de 65 de ani în intervalele orare 6:00-10:00, respectiv
+13:00-20:00;</label>
+<input type="checkbox" name="cb3"/>
+<label for="cb3">o deplasarea pentru <b>asistență medicală</b> care nu poate fi amânată și nici realizată de la
+distanță;</label>
+
+<input type="checkbox" name="cb4"/>
+<label for="cb4">deplasările scurte în apropierea locuinței/gospodăriei legate de <b>activitatea fizică
+individuală</b> a persoanelor (cu excluderea oricăror activități sportive de echipă), precum și
+pentru nevoile animalelor de companie/domestice;</label>
+
+<input type="checkbox" name="cb5"/>
+<label for="cb5">deplasarea în scopul <b>donării de sânge</b> la centrele de transfuzie sanguină;</label>
+
+<input type="checkbox" name="cb6"/>
+
+<label for="cb6">deplasarea în scop umanitar sau de <b>voluntariat</b>;</label>
+
+<input type="checkbox" name="cb7"/>
+
+<label for="cb7">deplasarea pentru realizarea de <b>activități agricole</b>;</label>
+
+<input type="checkbox" name="cb8"/>
+
+<label for="cb8">deplasarea <b>producătorilor agricoli</b> pentru comercializarea de produse agroalimentare;</label>
+
+<input type="checkbox" name="cb9"/>
+
+<label for="cb9">îngrijirea sau administrarea unei <b>proprietăți din altă localitate</b> și/sau eliberarea de
+documente necesare pentru obținerea unor drepturi;</label>
+
+
+<input type="checkbox" name="cb10"/>
+
+<label for="cb10">deplasare în vederea susținerii examenelor pentru obținerea <b>permisului de conducere</b>;</label>
+
+<input type="checkbox" name="cb11"/>
+
+<label for="cb11">participarea la programe sau proceduri în <b>centrele de tratament</b>;</label>
+
+<input type="checkbox" name="cb12"/>
+
+<label for="cb12">pentru achiziția, service-ul, efectuarea ITP sau alte operațiuni de <b>întreținere a vehiculelor</b>,
+activități care nu pot fi efectuate în localitatea de domiciliu, cu prezentarea unui document
+justificativ;</label>
+
+<input type="checkbox" name="cb13"/>
+
+<label for="cb13"><b>alte motive justificative</b> precum: îngrijirea/însoțirea copiilor/membrilor de familie, îngrijirea
+unei/unui rude/afin sau persoane aflate în întreținere, asistența persoanelor vârstnice, bolnave sau cu dizabilități ori deces al unui membru de familie;</label>
+<input type="checkbox" name="cb14"/>
+
+<label for="cb14">Alte motive legale:</label>
+
+<label for="adr" >Adresa spre care se deplasează:</label>
+<label for="time">Intervalul orar în care se efectuează deplasarea:</label>
+<label for="contact">Datele de identificare și de contact ale persoanei căreia i se acordă îngrijirea:</label>
+        </div>
+    )
+}
